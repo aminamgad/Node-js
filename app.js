fd42a6904461952d462248e8cf30742ee84edc31
Amin Amgad <96820371+aminamgad@users.noreply.github.com>
2023-02-28 22:06:43 +0200

    ok

--- app.js
+++ app.js
@@ -142,7 +142,6 @@ const { writeFileSync } = require('fs')
 for (let i = 0; i <= 10000; i++) {
    writeFileSync('./txt/first.txt' , `Amin Amgad  \t${i}\n` , {flag:'a'})
 }
-*/
 
 const {createReadStream} = require('fs')
 const stream = createReadStream('./txt/first.txt')
@@ -151,3 +150,128 @@ stream.on('data' , (result)=>{
    console.log(result);
 
 })
+
+const http = require('http')
+const {readFileSync} = require('fs')
+const homePage = readFileSync('./index.html')
+const aboutPage = readFileSync('./puplic/about.html')
+const server = http.createServer((req,res)=>{
+   const url = req.url
+
+if (url === '/') {
+   res.writeHead(200,{'content-type' : 'text/html'})
+   res.write(homePage)
+   res.end()
+}
+else if(url === '/about'){
+   res.writeHead(200,{'content-type' : 'text/html'})
+   res.write(aboutPage)
+   res.end()
+}
+else{
+   res.writeHead(200,{'content-type' : 'text/html'})
+   res.write('<h1>Page not found<h1>')
+   res.end()
+}
+
+})
+
+server.listen(5000,()=>{
+   console.log(' Server is run in http://localhost:5000')
+})
+
+
+*/
+
+//////EXPRESS//////////////////////
+const {products } = require('./data')
+
+const {readFileSync} = require('fs')
+const homePage = readFileSync('./index.html')
+const path = require('path')
+
+const express = require("express");
+const app = express();
+
+const logger = (req,res,next)=>{
+   const method = req.method
+   const url = req.url
+   const time = new Date().getFullYear()
+   console.log(method, url,time);
+   next()
+}
+
+
+app.use(express.static('./puplic'))
+
+app.get('/json',logger,(req,res)=>{
+   res.json([{name:'Amin' , id:1 , age:19},{name:'John' , age:20,id:2}])
+})
+app.get('/json2',logger,(req,res)=>{
+   const newProducts = products.map((product)=>{
+     // const name = product.name
+     const {id,name,price} = product;
+     return {id,name,price}
+   })
+   res.json(newProducts)
+})
+
+app.get('/json3/:productId',logger,(req,res)=>{
+const {productId} = req.params
+console.log(productId);
+
+   const singleProduct = products.find((profuct)=> profuct.id === Number(productId))
+
+   res.json(singleProduct)
+})
+
+app.get('/query' , logger,(req,res)=>{
+   const {search,limit} = req.query
+   let sortedProducts = [...products]
+
+   if (search) {
+      sortedProducts = sortedProducts.filter((product)=>{
+         return product.name.startsWith(search)
+      })
+   }
+   if (limit) {
+      sortedProducts = sortedProducts.slice(0,Number(limit))
+   }
+   res.status(200).json(sortedProducts)
+
+   console.log(req.query);
+  // res.send('Hello WOeld')
+})
+app.get("/",logger, (req, res) => {
+  res.status(200).send(
+    '<div style="margin:0;padding:0;background:#333;height:100vh;"><h1 style="color:aqua;">Home Page</h1><a href="/json2">Products</a></div>'
+  );
+});
+app.get("/amin",logger, (req, res) => {
+  res.status(200).write(homePage);
+  res.end()
+});
+app.get("/send", logger,(req, res) => {
+  res.sendFile(path.resolve(__dirname,'./puplic/about.html'))
+});
+
+app.get('/about' , logger,(req,res)=>{
+   res.status(200).send(    '<div style="margin:0;padding:0;background:#333;height:100vh;"><h1 style="color:aqua;">About Page</h1></div>'
+   )
+})
+
+app.all('*',logger,(req,res)=>{
+   res.status(404).send('Resource not found !')
+})
+
+app.listen(5000, () => {
+  [console.log("http://localhost:5000")];
+});
+
+//app.put()
+//app.get()
+//app.post()
+//app.delete()
+//app.all()
+//app.use()
+//app.listen()
---
+++ data.js
@@ -0,0 +1,24 @@
+const products = [
+    {id : 1,
+    name:"Amin Amgad",
+    price:'200$'
+    },
+    {id : 2,
+    name:"  Jasmine Amgad",
+    price:'20000$'
+    },
+    {id : 3,
+    name:"Ahmed Ramadan",
+    price:'2150$'
+    },
+    {id : 4,
+    name:"Islam Ibrahim",
+    price:'20$'
+    },
+    {id : 5,
+    name:"Ali Abo Alnasr",
+    price:'-1000$'
+    },
+]
+
+module.exports ={products}
